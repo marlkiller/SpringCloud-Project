1bfcaf0d5ae54f8271ed18de4d391939a45b2553
voidm <marlkiller@voidm.com>
2019-01-25 18:19:59 +0800

    fix: lambda语法

--- webflux-client/src/main/java/com/voidm/springcloud/controller/RoutingConfiguration.java
+++ webflux-client/src/main/java/com/voidm/springcloud/controller/RoutingConfiguration.java
@@ -1,10 +1,15 @@
 package com.voidm.springcloud.controller;
 
+import com.voidm.springcloud.doc.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
@@ -18,10 +23,16 @@ public class RoutingConfiguration {
     @Bean
     public RouterFunction<ServerResponse> monoRouterFunction(UserHandler userHandler) {
 
+
         return route(GET("/handler/users").and(accept(MediaType.APPLICATION_JSON)), userHandler::getAllUser)
                 .andRoute(GET("/handler/user/{id}").and(accept(MediaType.APPLICATION_JSON)), userHandler::getUserById)
                 .andRoute(POST("/handler/save").and(accept(MediaType.APPLICATION_JSON)), userHandler::saveUser)
-                .andRoute(GET("/handler/typeReference").and(accept(MediaType.APPLICATION_JSON)), userHandler::typeReference);
+                // .andRoute(GET("/handler/typeReference").and(accept(MediaType.APPLICATION_JSON)), userHandler::typeReference)
+                .andRoute(GET("/handler/typeReference").and(accept(MediaType.APPLICATION_JSON)), serverRequest -> {
+                    Map<Integer, Object> map = new HashMap<>(1);
+                    map.put(1, new User(1, "void"));
+                    return ServerResponse.ok().body(Mono.just(map), Map.class);
+                });
     }
 
 }
