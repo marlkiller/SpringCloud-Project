10ee640f69b00cc67c62035c7b8b00b1c9df097a
voidm <marlkiller@voidm.com>
2019-01-28 22:17:03 +0800

    fix: WebFlux 跨域问题解决

--- webflux-client/src/main/java/com/voidm/springcloud/WebFluxDemoApplication.java
+++ webflux-client/src/main/java/com/voidm/springcloud/WebFluxDemoApplication.java
@@ -3,6 +3,20 @@ package com.voidm.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 /**
  * WebFluxClient
@@ -15,4 +29,31 @@ public class WebFluxDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebFluxDemoApplication.class, args);
     }
+
+    @Bean
+    public WebFilter corsFilter() {
+        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+            ServerHttpRequest request = ctx.getRequest();
+            if (CorsUtils.isCorsRequest(request)) {
+                ServerHttpResponse response = ctx.getResponse();
+                HttpHeaders headers = response.getHeaders();
+                headers.add("Access-Control-Allow-Origin", "*");
+                headers.add("Access-Control-Allow-Methods", "*");
+                headers.add("Access-Control-Max-Age", "18000L");
+                headers.add("Access-Control-Allow-Headers", "*");
+                headers.add("Access-Control-Expose-Headers", "*");
+                headers.add("Access-Control-Allow-Credentials", "true");
+                if (request.getMethod() == HttpMethod.OPTIONS) {
+                    response.setStatusCode(HttpStatus.OK);
+                    return Mono.empty();
+                }
+            }
+            return chain.filter(ctx);
+        };
+    }
+    // 如果使用了注册中心（如：Eureka），进行控制则需要增加如下配置
+    // @Bean
+    // public RouteDefinitionLocator discoveryClientRouteDefinitionLocator(DiscoveryClient discoveryClient) {
+    //     return new DiscoveryClientRouteDefinitionLocator(discoveryClient);
+    // }
 }
