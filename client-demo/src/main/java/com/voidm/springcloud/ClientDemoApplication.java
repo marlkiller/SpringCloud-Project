a21326def9b46b543c94f6a98baa8568cc4dd75e
voidm <marlkiller@voidm.com>
2019-01-28 22:00:34 +0800

    fix: 跨域问题解决

--- client-demo/src/main/java/com/voidm/springcloud/ClientDemoApplication.java
+++ client-demo/src/main/java/com/voidm/springcloud/ClientDemoApplication.java
@@ -3,6 +3,10 @@ package com.voidm.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * 内部调用服务
@@ -19,5 +23,24 @@ public class ClientDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClientDemoApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // 对接口配置跨域设置
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 允许任何域名使用
+        corsConfiguration.addAllowedOrigin("*");
+        // 允许任何头
+        corsConfiguration.addAllowedHeader("*");
+        // 允许任何方法（post、get等）
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
     }
 }
